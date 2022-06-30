9460093273d044e6fd62ca23430e34c66eb28436
DrewSnarski <asnarski1@hawk.iit.edu>
2022-06-30 13:52:35 -0500

    Jenkins install fix

--- scripts/proxmox/post_install_prxmx_ubuntu_2004.sh
+++ scripts/proxmox/post_install_prxmx_ubuntu_2004.sh
@@ -18,16 +18,18 @@ sudo rm -f /etc/machine-id
 sudo touch /etc/machine-id
 
 
+# Install Java
+sudo apt update
+sudo apt install -y openjdk-11-jdk
+
 # Install Jenkins
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \
  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
 sudo apt-get install -y jenkins 
-
-# Install Java
-sudo apt install -y openjdk-11-jre
 
 # Start Jenkins
 sudo systemctl start jenkins
